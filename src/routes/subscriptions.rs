b4df89f24c846d62685579f2b092752e1424383c
yanzixian <yanzx73@163.com>
2025-04-06 22:50:58 +0800

    4.5.4

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,6 +2,7 @@ use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
 use uuid::Uuid;
+use tracing::Instrument;
 #[derive(serde::Deserialize)]
 pub struct FormData {
     email: String,
@@ -18,17 +19,9 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     );
 
     let _request_span=request_span.enter();
-    tracing::info!(
-        "request_id {} - Add '{}' '{}'  as a new subscriber",
-        request_id,
-        form.email,
-        form.name
-    );
-    tracing::info!(
-        "request_id {} - Saving new subscriber details into the database",
-        request_id);
+    let query_span=tracing::info_span!("Saving a new subscriber in the database.");
 
-    match sqlx::query!(
+      match sqlx::query!(
         r#"INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4)"#,
         Uuid::new_v4(),
         form.email,
@@ -36,10 +29,10 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         Utc::now()
     )
     .execute(pool.get_ref())
+    .instrument(query_span)
     .await
     {
         Ok(_) => {
-            tracing::info!("request_id {} - New subscriber details have been saved.", request_id);
             HttpResponse::Ok().finish()
         },
         Err(e) => {
